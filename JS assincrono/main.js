e7a4cdf27551a7eecc9cfec7dfa3f4ef24390a09
unknown <kaah.fernandess@hotmail.com>
2019-11-27 19:27:22 -0300

    Modulo - 04: Exercicio 01

--- JS assincrono/main.js
+++ JS assincrono/main.js
@@ -1,7 +1,20 @@
-axios.get('https://api.github.com/users/milafrn')
-    .then(function(response){
-        console.log(response);
+
+function checaIdade(idade) {
+    return new Promise((resolve, reject) => {
+        if (idade >= 18) {
+            setTimeout(resolve, 2000, idade);
+        } else {
+            setTimeout(reject, 2000, idade);
+        }
     })
-    .catch(function(error){
-        console.warn(error);
-    });
+}
+
+
+checaIdade(15)
+    .then(function () {
+        console.log("Maior que 18");
+    })
+    .catch(function () {
+        console.log("Menor que 18");
+
+    })
