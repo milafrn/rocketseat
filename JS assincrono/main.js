9a041c12ef435f9de72e446e25fc854c0825694a
unknown <kaah.fernandess@hotmail.com>
2019-11-27 18:08:01 -0300

    Modulo - 04: Learning Axios

--- JS assincrono/main.js
+++ JS assincrono/main.js
@@ -1,22 +1,4 @@
-let minhaPromise = function(){
-    return new Promise(function(resolve, reject){
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/milafrn');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/milafrn')
     .then(function(response){
         console.log(response);
     })
